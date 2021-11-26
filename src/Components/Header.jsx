8696416f4e2521deb88fa33e6beb5e5257ef84d8
hariscs4997 <hariscs4997@gmail.com>
2021-11-26 16:22:33 +0500

    timeframe updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,19 +20,10 @@ const Header = ({
 }) => {
   let [stocks, setStocks] = useState([]);
   const times = [
-    "1m",
-    "2m",
-    "5m",
-    "15m",
-    "30m",
-    "60m",
-    "90m",
     "1h",
     "1d",
-    "5d",
     "1wk",
     "1mo",
-    "3mo",
   ];
   useEffect(() => {
     getAllStocks("stocks/available").then((res) => {
